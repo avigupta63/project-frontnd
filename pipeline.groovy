0a7a14ee71f072ed3be4e69e8de2a43268d6da68
avigupta63 <avi.gupta63@gmail.com>
2025-05-08 08:09:51 +0000

    frontend deploy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,20 +6,28 @@ pipeline {
                 git branch: 'dev', url: 'https://github.com/avigupta63/project-frontnd.git'
             }
         }
+
         stage('code-build') {
             steps {
                 sh '''
-                     npm install
-                     ng build
-                   '''
+                    npm install
+                    npx ng build --configuration production
+                '''
             }
         }
-      
+
         stage('code-deploy') {
             steps {
-                sh '''
-                     aws s3 cp --recursive dist/angular-frontend s3://mywasbucket321/
-                   '''
+                withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    credentialsId: 'aws_creds',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                ]]) {
+                    sh '''
+                        aws s3 cp --recursive dist/angular-frontend s3://mywasbucket321/
+                    '''
+                }
             }
         }
     }
